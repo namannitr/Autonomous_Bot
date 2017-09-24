effddc0cc55bf5d7e9dd6aa57e0afccec1f60887
namannitr <namannitr@gmail.com>
2017-09-24 10:22:40 +0530

    Removing Whatsapp dependency

--- processMessage/pickMessage/pick.py
+++ processMessage/pickMessage/pick.py
@@ -5,28 +5,28 @@ from brain.brain import Brain
 from bot.session import Session
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from databaseImport.databaseUpdate import DbUpdate as dbUpdate
+from dumpMessage.dump import Dump
 
 
 #class PickMessage(object):
 
 
-def pickAndProcess(dbUpdateObject,sessionObject,brainObject):
+def pickAndProcess(dbUpdateObject,sessionObject,brainObject, dumpObject):
 	l=dbUpdateObject.selectResponseMessageLastEntry()
 	if(l!=None):
 		l=dbUpdateObject.selectReceiveMessageById(str(l[0]));
-	else:	
+	else:
 		l=dbUpdateObject.selectReceiveMessageAll();
 	for item in l:
-		brainObject.brain1(dbUpdateObject,sessionObject,item)
-			
+		brainObject.brain1(dbUpdateObject,sessionObject,item, dumpObject)
+
 if __name__ =="__main__":
 	dbUpdateObject=dbUpdate()
 	sessionObject=Session();
 	brainObject=Brain();
+	dumpObject=Dump();
 	#pickObject=PickMessage();
 	sessionObject.learn();
 	while(1):
-		pickAndProcess(dbUpdateObject,sessionObject,brainObject);
+		pickAndProcess(dbUpdateObject,sessionObject,brainObject, dumpObject);
 		time.sleep(1);
-		
-
